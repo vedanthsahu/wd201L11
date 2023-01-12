064d8ec8aff2e4a85daf7ec154b766a4d9aa6ee3
vedanthsahu <vedanthsahu100@gmail.com>
2023-01-12 12:33:46 +0530

    UPDATED

--- migrations/20230109012547-create-voter-rel.js
+++ migrations/20230109012547-create-voter-rel.js
@@ -1,15 +1,6 @@
 ("use strict");
 /** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("VoterRels", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      voterid: {
+modu
         type: Sequelize.STRING,
         allowNull: false,
         unique: true,
