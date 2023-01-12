7a4aa00684bd87ba09c161f13719f90a5a224129
vedanthsahu <vedanthsahu100@gmail.com>
2023-01-12 12:33:07 +0530

    UPDATED

--- migrations/20230106165946-create-options.js
+++ migrations/20230106165946-create-options.js
@@ -2,7 +2,14 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await que
+    await queryInterface.createTable("Options", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      option: {
         type: Sequelize.STRING,
         allowNull: false,
       },
