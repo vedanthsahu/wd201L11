ad30db792ba465bccf226957af4f65bb96177a5c
vedanthsahu <vedanthsahu100@gmail.com>
2023-01-12 12:31:45 +0530

    UPDATED

--- migrations/20230105133037-add-adminid-in-election.js
+++ migrations/20230105133037-add-adminid-in-election.js
@@ -14,12 +14,7 @@ module.exports = {
         field: "id",
       },
     });
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+
   },
 
   async down(queryInterface, Sequelize) {
