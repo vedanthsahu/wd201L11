154f2a9b4a0e9f425535db3bdf22e5614af17d0b
vedanthsahu <vedanthsahu100@gmail.com>
2023-01-12 12:33:21 +0530

    UPDATED

--- migrations/20230106170014-add-questionId-in-options.js
+++ migrations/20230106170014-add-questionId-in-options.js
@@ -1,16 +1,7 @@
 "use strict";
 
 /** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn("Options", "questionId", {
-      type: Sequelize.DataTypes.INTEGER,
-      onDelete: "CASCADE",
-    });
-
-    await queryInterface.addConstraint("Options", {
-      fields: ["questionId"],
-      type: "foreign key",
+module.ex
       onDelete: "CASCADE",
       references: {
         table: "questions",
