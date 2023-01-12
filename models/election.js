a13f8a326ec66dca3cbc59fb2aea68a38631ca49
vedanthsahu <vedanthsahu100@gmail.com>
2023-01-12 12:35:00 +0530

    UPDATED

--- models/election.js
+++ models/election.js
@@ -1,15 +1,6 @@
 "use strict";
 const { Model } = require("sequelize");
-module.exports = (sequelize, DataTypes) => {
-  class Election extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static createElection({ electionName, adminID, publicurl }) {
-      let createElection = this.create({
-        electionName,
+module.e
         publicurl,
         adminID,
       });
